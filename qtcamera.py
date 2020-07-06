a63b2d36678b053711c11e39a12d3009363e3753
Bastian Bechtold <basti@bastibe.de>
2020-07-06 14:31:29 +0200

    borderless fullscreen window

--- qtcamera.py
+++ qtcamera.py
@@ -7,36 +7,48 @@ import time
 import sys
 
 class Viewfinder(QtWidgets.QWidget):
-    def __init__(self, parent):
+    closeWindow = QtCore.Signal()
+
+    def __init__(self, parent, width, height):
         super().__init__(parent)
+        self.size = (width, height)
+
         # initialize the camera:
         self.camera = PiCamera()
-        self.camera.resolution = (640, 480)
+        self.camera.resolution = self.size
         self.camera.framerate = 60
 
     def paintEvent(self, event):
         with PiRGBArray(self.camera) as output:
             self.camera.capture(output, 'rgb', use_video_port=True)
-            image = QtGui.QImage(output.array, 640, 480, 640*3, QtGui.QImage.Format_RGB888)
+            image = QtGui.QImage(output.array, *self.size, self.size[0]*3, QtGui.QImage.Format_RGB888)
 
         painter = QtGui.QPainter(self)
         painter.drawImage(self.rect(), image)
         self.update()
 
-    def sizeHint(self):
-        return QtCore.QSize(640, 480)
+    def mousePressEvent(self, event):
+        self.closeWindow.emit()
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class FullScreenWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init__(flags=QtCore.Qt.FramelessWindowHint)
         self.setWindowTitle("Viewfinder")
 
-        main_widget = Viewfinder(self)
+        screensize = app.desktop().availableGeometry()
+        screenwidth = screensize.x() + screensize.width()
+        screenheight = screensize.y() + screensize.height()
+
+        main_widget = Viewfinder(self, screenwidth, screenheight)
         self.setCentralWidget(main_widget)
+        main_widget.closeWindow.connect(self.close)
+
+        self.setGeometry(QtCore.QRect(0, 0, screenwidth, screenheight))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    window = FullScreenWindow()
     window.show()
     sys.exit(app.exec_())
